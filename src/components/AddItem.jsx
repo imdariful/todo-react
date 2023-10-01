46d04ba8840e570b40d8e3ea1a212077e4b83f1f
Ariful Islam <ariful.rony10@gmail.com>
2023-10-01 23:24:45 +0600

    useRef() implemented for shifting focus

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -1,11 +1,14 @@
 import { useRef } from "react";
 import { FaPlus } from "react-icons/fa";
 function AddItem({ newItem, setNewItem, handleAddNewItemSubmit }) {
+  const inputRef = useRef(); // creating a reference 'inputRef'
+
   return (
     <form className="addForm" onSubmit={handleAddNewItemSubmit}>
       <label htmlFor="addItem">Add Item</label>
       <input
         autoFocus
+        ref={inputRef} // attached the defined reference to this input
         id="addItem"
         type="text"
         placeholder="Add Item"
@@ -13,7 +16,11 @@ function AddItem({ newItem, setNewItem, handleAddNewItemSubmit }) {
         value={newItem}
         onChange={(e) => setNewItem(e.target.value)}
       />
-      <button type="submit" aria-label="Add Item">
+      <button
+        type="submit"
+        aria-label="Add Item"
+        onClick={() => inputRef.current.focus()} // this code programmatically gives focus to the input element referenced by inputRef
+      >
         <FaPlus />
       </button>
     </form>
